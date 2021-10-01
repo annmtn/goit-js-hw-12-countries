e94352d4d644b8edb1914834c7aec0bf8fed2dc3
Anna Mitina <anndan5810@gmail.com>
2021-10-01 14:18:44 +0300

    ссылки

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export default {
+    searchQuery: document.querySelector('#input'),
+    countryListEl: document.querySelector('js-country-list'),
+    wrapperEl: document.querySelector('wrapper'),
+
+}
