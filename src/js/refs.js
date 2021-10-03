ccd524063c9491eac72fe812ea08f09095c4c608
Anna Mitina <anndan5810@gmail.com>
2021-10-03 03:18:45 +0300

    finish

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,5 @@
 export default {
-    searchQuery: document.querySelector('#input'),
-    countryListEl: document.querySelector('js-country-list'),
-    wrapperEl: document.querySelector('wrapper'),
-
-}
+  searchQuery: document.querySelector('#input'),
+  countryListEl: document.querySelector('.js-country-list'),
+  wrapperEl: document.querySelector('.wrapper'),
+};
